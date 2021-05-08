d897bcca79c7b28eb6399a8910bc150fd15bbe4f
Tamal Saha <tamal@appscode.com>
2021-05-08 09:51:15 -0700

    added-all

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/tamalsaha/status-equality-check
 
 go 1.16
+
+require (
+	github.com/fatih/structs v1.1.0
+	k8s.io/apimachinery v0.21.0
+	k8s.io/client-go v0.21.0
+	k8s.io/klog/v2 v2.8.0
+	kmodules.xyz/client-go v0.0.0-20210505231546-fa4fb8e1d04e
+)
--- main.go
+++ main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"fmt"
+	meta_util "kmodules.xyz/client-go/meta"
+	"reflect"
+
+	"github.com/fatih/structs"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
 func main() {
 
+}
+
+func statusEqual(old, new interface{}) bool {
+	oldStatus, oldExists := extractStatusFromObject(old)
+	newStatus, newExists := extractStatusFromObject(new)
+	if oldExists && newExists {
+		result := reflect.DeepEqual(oldStatus, newStatus)
+		if !result {
+			diff, err := meta_util.JsonDiff(oldStatus, newStatus)
+			if err == nil {
+				fmt.Println(diff)
+			} else {
+				panic(err)
+			}
+		}
+		return result
+	}
+	return !oldExists && !newExists
+}
+
+func extractStatusFromObject(o interface{}) (interface{}, bool) {
+	switch obj := o.(type) {
+	case *unstructured.Unstructured:
+		v, ok, _ := unstructured.NestedFieldNoCopy(obj.Object, "status")
+		return v, ok
+	case metav1.Object:
+		st := structs.New(obj)
+		field, ok := st.FieldOk("Status")
+		if !ok {
+			return nil, ok
+		}
+		return field.Value(), true
+	}
+	panic(fmt.Errorf("unknown object %v", reflect.TypeOf(o)))
 }
